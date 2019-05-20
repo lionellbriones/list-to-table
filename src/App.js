3c0c8e03cbddfcaecc593500bd9a379f0c020b57
Lionell Briones <llenoil@gmail.com>
2019-05-20 16:07:35 +0800

    Added the list app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    list: "",
+    placeList: []
+  };
+
+  chunk(array, size) {
+    const chunked_arr = [];
+    for (let i = 0; i < array.length; i++) {
+      const last = chunked_arr[chunked_arr.length - 1];
+      if (!last || last.length === size) {
+        chunked_arr.push([array[i]]);
+      } else {
+        last.push(array[i]);
+      }
+    }
+    return chunked_arr;
+  }
+
+  handleListChange = event => {
+    this.setState({
+      list: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    let placeList = this.state.list;
+    placeList = placeList.split("\n");
+    placeList = this.chunk(placeList, 4);
+
+    this.setState({
+      placeList: placeList
+    });
+  };
+
+  displayList = event => {
+    let tr = [];
+
+    this.state.placeList.forEach((item, id) => {
+      let row = [];
+      item.forEach((col, colId) => {
+        row.push(<td key={colId}>{col}</td>);
+      });
+
+      tr.push(<tr key={id}>{row}</tr>);
+    });
+
+    return tr;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <textarea
+          value={this.state.list}
+          onChange={this.handleListChange}
+          cols="30"
+          rows="10"
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Transform</button>
+
+        <hr />
+        <br />
+        <br />
+
+        <table>
+          <tbody>{this.displayList()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
